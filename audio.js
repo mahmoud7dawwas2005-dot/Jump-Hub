7c3b6fa6bab539fb083d34606174fe1f67daff79
mahmoud7dawwas2005-dot <mahmoud7dawwas2005@gmail.com>
2026-01-02 01:54:06 +0200

    Create audio.js

---
+++ audio.js
@@ -0,0 +1,9 @@
+const audio = {
+  jump: new Audio("assets/sounds/jump.wav"),
+  fall: new Audio("assets/sounds/fall.wav"),
+  music: new Audio("assets/sounds/music.mp3"),
+  ad: new Audio("assets/sounds/ad.wav")
+};
+
+audio.music.loop = true;
+audio.music.volume = 0.4;
